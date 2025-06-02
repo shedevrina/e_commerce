6e782555b54ceda8fc93cd3794b77e29c936bf1c
Anastasiya Shevrina <shevrina@inbox.ru>
2025-06-02 23:09:46 +0700

    Tests file for homework_14_1

---
+++ test/test_product.py
@@ -0,0 +1,6 @@
+
+def test_product_init(product):
+    assert product.name == "name_test"
+    assert product.description == "description_test"
+    assert product.price == 100.00
+    assert product.quantity == 15
