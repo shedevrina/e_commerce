57a7b8941b6dbe219269fccf9c14d284ee179e99
Anastasiya Shevrina <shevrina@inbox.ru>
2025-06-02 23:08:36 +0700

    Moduls for homework_14_1

---
+++ src/category.py
@@ -0,0 +1,21 @@
+from src.product import Product
+
+
+class Category:
+    """Класс для представления категории продуктов"""
+
+    name: str
+    description: str
+    products: list[Product]
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products=None):
+
+        self.name = name
+        self.description = description
+        self.products = products if products else []
+
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
